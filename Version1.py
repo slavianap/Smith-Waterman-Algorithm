f31e3668af5f618425cc527fbc61a7d94ecd096f
Slaviana Pavlovich <zceipav@ucl.ac.uk>
2020-04-18 05:34:36 +0100

    Add files via upload

--- Version1.py
+++ Version1.py
@@ -13,40 +13,10 @@ Task: Smith Waterman (SW) local alignment
 # Importing Python packages
 import numpy as np
 
-# Score constants
+# Defining the constants for the scores
 MATCH = 1
 MISMATCH = -1
 GAP = -1
-
-# Creating the scoring matrix with the given scores for match, mismatch and gap
-def scoring_matrix(seq1, seq2):
-    # Initialising a NumPy zeros array with the shape of (the first
-    # sequence + 1 and the second sequence + 1) and of the integer data type)
-    row = len(seq1) + 1
-    col = len(seq2) + 1
-    matrix = np.zeros(shape = (row, col), dtype = 'int')    
-    
-    # The score of the cell in the matrix depends on the three cells' values:
-    # on the left, above and diagonal (relative to the cell)
-    for i in range(1, row):
-        for j in range(1, col):
-            # Calculating the diagonal score
-            match_value = MATCH if seq1[i - 1] == seq2[j - 1] else MISMATCH
-            diagonal_score = matrix[i - 1, j - 1] + match_value
-            
-            # Calculating the score above the cell
-            above_score = matrix[i - 1, j] + GAP
-            
-            # Calculating the score on the left
-            left_score = matrix[i, j - 1] + GAP
-            
-            # The maximum score is then picked and stored in the cell of the matrix
-            matrix[i, j] = max(0, diagonal_score, above_score, left_score)
-    return matrix
-            
-
-#def backtrack()            
-  
 
 # Reading the fasta file and keeping the required sequence's name and sequence
 def fasta_reader(sequence_file):
@@ -54,10 +24,34 @@ def fasta_reader(sequence_file):
     sequence_name_row = lines[0][1:]
     sequence = lines[1]
     return sequence_name_row.replace(" ", "").strip(), sequence.strip()
-        
 
+# Creating the scoring matrix
+def scoring_matrix(seq1, seq2):
+    row = len(seq1) + 1
+    col = len(seq2) + 1
+    matrix = np.zeros(shape = (row, col), dtype = 'int')    
+    for i in range(1, row):
+        for j in range(1, col):
+            # Calculating the diagonal score (match score)
+            match_value = MATCH if seq1[i - 1] == seq2[j - 1] else MISMATCH
+            diagonal_score = matrix[i - 1, j - 1] + match_value
+            
+            # Calculating the vertical gap score
+            vertical_score = matrix[i - 1, j] + GAP
+            
+            # Calculating the horizontal gap score
+            horizontal_score = matrix[i, j - 1] + GAP
+            
+            # Taking the highest score 
+            matrix[i, j] = max(0, diagonal_score, vertical_score, horizontal_score)
+    return matrix
+            
+#def traceback()            
+
+        
 # Importing two required fasta sequences
 file_1_name, file_1 = fasta_reader("Sequence1.fasta")
 file_2_name, file_2  = fasta_reader("Sequence2.fasta")
+
 # Running the Smith Waterman local alignment
 M = scoring_matrix(file_1, file_2)
