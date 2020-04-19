30c756cfb101a826e777387623fa4d3d4de32599
Slaviana Pavlovich <zceipav@ucl.ac.uk>
2020-04-19 04:25:02 +0100

    Add files via upload

--- Version1.py
+++ Version1.py
@@ -4,19 +4,28 @@
 
 LIFE97011 - Computing
 Python Programming - Assessed Exercise No. 3
-Task: Smith Waterman (SW) local alignment
+Task: Smith Waterman local alignment
 @Author: Slaviana Pavlovich
 @CID: 01739756
 
 """
 
 # Importing Python packages
+from enum import IntEnum
 import numpy as np
 
-# Defining the constants for the scores
-MATCH = 1
-MISMATCH = -1
-GAP = -1
+# Assigning the constants for the scores
+class Score(IntEnum):
+    MATCH = 1
+    MISMATCH = -1
+    GAP = -1
+
+# Assigning the constant values for the traceback
+class Trace(IntEnum):
+    STOP = 0
+    LEFT = 1 
+    UP = 2
+    DIAGONAL = 3
 
 # Reading the fasta file and keeping the formatted sequence's name and sequence
 def fasta_reader(sequence_file):
@@ -25,63 +34,77 @@ def fasta_reader(sequence_file):
     sequence = lines[1]
     return sequence_name_row.replace(" ", "").strip(), sequence.strip()
 
-# Implementing the Smith Waterman (SW) local alignment
-def SW(seq1, seq2):
-    maximum_score = 0
-    STOP, LEFT, UP, DIAGONAL = range(4)
-    aligned_seq1 = ""
-    aligned_seq2 = ""    
-    
-    # Creating the empty matrices for storing scores and tracing
+# Implementing the Smith Waterman local alignment
+def smith_waterman(seq1, seq2):
+    # Generating the empty matrices for storing scores and tracing
     row = len(seq1) + 1
     col = len(seq2) + 1
-    matrix = np.zeros(shape = (row, col), dtype = 'int')  
-    tracing_matrix = np.zeros(shape = (row, col), dtype = 'int')  
+    matrix = np.zeros(shape=(row, col), dtype=np.int)  
+    tracing_matrix = np.zeros(shape=(row, col), dtype=np.int)  
+    
+    # Initialising the variables to find the highest scoring cell
+    max_score = -1
+    max_index = (-1, -1)
     
     # Calculating the scores for all cells in the matrix
     for i in range(1, row):
         for j in range(1, col):
             # Calculating the diagonal score (match score)
-            match_value = MATCH if seq1[i - 1] == seq2[j - 1] else MISMATCH
+            match_value = Score.MATCH if seq1[i - 1] == seq2[j - 1] else Score.MISMATCH
             diagonal_score = matrix[i - 1, j - 1] + match_value
+            
             # Calculating the vertical gap score
-            vertical_score = matrix[i - 1, j] + GAP
+            vertical_score = matrix[i - 1, j] + Score.GAP
+            
             # Calculating the horizontal gap score
-            horizontal_score = matrix[i, j - 1] + GAP
+            horizontal_score = matrix[i, j - 1] + Score.GAP
+            
             # Taking the highest score 
             matrix[i, j] = max(0, diagonal_score, vertical_score, horizontal_score)
+            
             # Tracking where the cell's value is coming from    
-            if matrix[i][j] == 0: 
-                tracing_matrix[i][j] = STOP
-            if matrix[i][j] == horizontal_score: 
-                tracing_matrix[i][j] = LEFT
-            if matrix[i][j] == vertical_score: 
-                tracing_matrix[i][j] = UP
-            if matrix[i][j] == diagonal_score: 
-                tracing_matrix[i][j] = DIAGONAL 
+            if matrix[i, j] == 0: 
+                tracing_matrix[i, j] = Trace.STOP
+                
+            elif matrix[i, j] == horizontal_score: 
+                tracing_matrix[i, j] = Trace.LEFT
+                
+            elif matrix[i, j] == vertical_score: 
+                tracing_matrix[i, j] = Trace.UP
+                
+            elif matrix[i, j] == diagonal_score: 
+                tracing_matrix[i, j] = Trace.DIAGONAL 
+                
             # Tracking the cell with the maximum score
-            if matrix[i][j] >= maximum_score:
-                maximum_i = i
-                maximum_j = j
-                maximum_score = matrix[i][j]
-           
-    # Assigning to i and j the cell's position with the highest score
-    i, j = maximum_i, maximum_j
+            if matrix[i, j] >= max_score:
+                max_index = (i,j)
+                max_score = matrix[i, j]
     
-    while tracing_matrix[i, j] != STOP:
-        if tracing_matrix[i, j] == DIAGONAL:
-            current_aligned_seq1 = seq1[i - 1]
-            current_aligned_seq2 = seq2[j - 1]
-            i = i - 1
-            j = j - 1
-        elif tracing_matrix[i, j] == UP:
-            current_aligned_seq1 = seq1[i - 1]
+    # Initialising the variables for tracing
+    aligned_seq1 = ""
+    aligned_seq2 = ""   
+    current_aligned_seq1 = ""   
+    current_aligned_seq2 = ""  
+    (max_i, max_j) = max_index
+    
+    # Tracing and computing the pathway with the local alignment
+    while tracing_matrix[max_i, max_j] != Trace.STOP:
+        if tracing_matrix[max_i, max_j] == Trace.DIAGONAL:
+            current_aligned_seq1 = seq1[max_i - 1]
+            current_aligned_seq2 = seq2[max_j - 1]
+            max_i = max_i - 1
+            max_j = max_j - 1
+            
+        elif tracing_matrix[max_i, max_j] == Trace.UP:
+            current_aligned_seq1 = seq1[max_i - 1]
             current_aligned_seq2 = '-'
-            i = i - 1            
-        elif tracing_matrix[i, j] == LEFT:
+            max_i = max_i - 1    
+            
+        elif tracing_matrix[max_i, max_j] == Trace.LEFT:
             current_aligned_seq1 = '-'
-            current_aligned_seq2 = seq2[j - 1]
-            j = j - 1
+            current_aligned_seq2 = seq2[max_j - 1]
+            max_j = max_j - 1
+            
         aligned_seq1 = aligned_seq1 + current_aligned_seq1
         aligned_seq2 = aligned_seq2 + current_aligned_seq2
     
@@ -89,12 +112,14 @@ def SW(seq1, seq2):
     aligned_seq1 = aligned_seq1[::-1]
     aligned_seq2 = aligned_seq2[::-1]
     
-    return(aligned_seq1, aligned_seq2)
+    return aligned_seq1, aligned_seq2
 
-# Importing two required fasta sequences
-file_1_name, file_1 = fasta_reader("Sequence1.fasta")
-file_2_name, file_2  = fasta_reader("Sequence2.fasta")
-
-# Running the Smith Waterman local alignment and displaying the alignment
-output_1, output_2 = SW(file_1, file_2)
-print(file_1_name + ' ' + output_1 + '\n' + file_2_name + ' ' + output_2)
+if __name__ == "__main__":
+    # Reading the two required fasta sequences
+    file_1_name, file_1 = fasta_reader("Sequence1.fasta")
+    file_2_name, file_2  = fasta_reader("Sequence2.fasta")
+    
+    # Executing the Smith Waterman local alignment algorithm
+    output_1, output_2 = smith_waterman(file_1, file_2)
+    
+    print(file_1_name + ' ' + output_1 + '\n' + file_2_name + ' ' + output_2)
